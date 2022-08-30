71ba35bf6f26a38243d534357a6f81828a666819
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-08-30 07:49:24 +0000

    update params.sh to match nsb environment params

--- params.sh
+++ params.sh
@@ -3,20 +3,20 @@
 # Common params
 LICENSE_USE=production
 DOMAIN=nsb.local
-DNS_SERVERS='10.1.2.5'
-SSH_KEY='~/.ssh/id_rsa.pub'
+DNS_SERVERS='10.1.220.74,10.1.220.75,10.1.220.76,172.17.220.75'
+SSH_KEY='~/.ssh/id_rsa.1.pub'
 PASSWORD=${PASSWORD:-test@12345}
-SUBNET_MASK=255.255.255.0                                                                                                          
-GATEWAY="192.168.70.1"
+SUBNET_MASK=255.255.255.128
+GATEWAY="192.168.72.1"
 
 # Management subsystem
-MGMT_HOST_IP="192.168.70.11"
+MGMT_HOST_IP="192.168.72.11"
 MGMT_HOSTNAME=pr-apic-mgr
 
 # Analytics subsystem
-ANALYT_HOST_IP="192.168.70.12"
+ANALYT_HOST_IP="192.168.72.12"
 ANALYT_HOSTNAME=pr-apic-anytcs
 
 # Developer portal subsystem
-PORT_HOST_IP="192.168.70.13"
+PORT_HOST_IP="192.168.72.13"
 PORT_HOSTNAME=pr-apic-devptl
