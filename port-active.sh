0b9ef6ee7083cafd1660a291406ba4c0c69c48c2
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-10-21 16:44:10 +0000

    remove hot standby configuration

--- port-active.sh
+++ port-active.sh
@@ -5,12 +5,4 @@ SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ${SCRIPT_ROOT}/site-active-params.sh
 source ${SCRIPT_ROOT}/port.sh
 
-apicup subsys set $PORT_NAME multi-site-ha-enabled=true
-apicup subsys set $PORT_NAME multi-site-ha-mode=active
-apicup certs set $PORT_NAME portal-replication-ingress --clear
-apicup subsys set $PORT_NAME portal-replication="ptlreplication${ACTIVE_SITE_NAME}.apic${ACTIVE_SITE_NAME}.${DOMAIN}"
-apicup subsys set $PORT_NAME replication-peer-fqdn="ptlreplication${PASSIVE_SITE_NAME}.apic${PASSIVE_SITE_NAME}.${DOMAIN}"
-apicup subsys set $PORT_NAME site-name=$ACTIVE_SITE_NAME
-
 apicup subsys install $PORT_NAME --out "${PORT_NAME}-plan-out"
-apicup certs get $PORT_NAME encryption-secret -t key > port-encryption-secret
--- port-passive.sh
+++ port-passive.sh
@@ -5,13 +5,4 @@ SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ${SCRIPT_ROOT}/site-passive-params.sh
 source ${SCRIPT_ROOT}/port.sh
 
-apicup subsys set $PORT_NAME multi-site-ha-enabled=true
-apicup subsys set $PORT_NAME multi-site-ha-mode=passive
-apicup certs set $PORT_NAME portal-replication-ingress --clear
-apicup subsys set $PORT_NAME portal-replication="ptlreplication${PASSIVE_SITE_NAME}.apic${PASSIVE_SITE_NAME}.${DOMAIN}"
-apicup subsys set $PORT_NAME replication-peer-fqdn="ptlreplication${ACTIVE_SITE_NAME}.apic${ACTIVE_SITE_NAME}.${DOMAIN}"
-apicup subsys set $PORT_NAME site-name=$PASSIVE_SITE_NAME
-
-apicup certs generate $PORT_NAME
-apicup certs set $PORT_NAME encryption-secret port-encryption-secret
 apicup subsys install $PORT_NAME --out "${PORT_NAME}-plan-out"
