9ec7b6c6eef56f99cb7ca576c2225482bd264074
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-10-24 10:12:31 +0000

    fix auto.sh automating running individual scripts

--- auto.sh
+++ auto.sh
@@ -3,8 +3,12 @@
 PROJECT_NAME="$1"
 
 SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source ${SCRIPT_ROOT}/init.sh "$PROJECT_NAME"
-source ${SCRIPT_ROOT}/mgmt.sh
-source ${SCRIPT_ROOT}/analyt.sh
-source ${SCRIPT_ROOT}/port.sh
+${SCRIPT_ROOT}/init.sh "$PROJECT_NAME"
+cd "$PROJECT_NAME"
+${SCRIPT_ROOT}/mgmt-active.sh
+${SCRIPT_ROOT}/analyt-active.sh
+${SCRIPT_ROOT}/port-active.sh
+${SCRIPT_ROOT}/mgmt-passive.sh
+${SCRIPT_ROOT}/analyt-passive.sh
+${SCRIPT_ROOT}/port-passive.sh
 
