6fac4f02a0853bc57843a934f88db91ae864ce46
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-11-11 08:21:14 +0000

    delete unnecessary files for storage replication DR

--- mgmt-passive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-source ${SCRIPT_ROOT}/site-passive-params.sh
-source ${SCRIPT_ROOT}/mgmt.sh
-
-apicup subsys install $MGMT_NAME --out "${MGMT_NAME}-plan-out"
--- port-passive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-source ${SCRIPT_ROOT}/site-passive-params.sh
-source ${SCRIPT_ROOT}/port.sh
-
-apicup subsys install $PORT_NAME --out "${PORT_NAME}-plan-out"
